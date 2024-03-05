3061cdbe2180c99e18ba1ee161258655e582235d
vladislav <vladislav@example.com>
2024-03-05 23:32:00 +0100

    added new battles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-
 var elements = document.getElementsByClassName('block-hidden');
+var markerIds = document.getElementsByClassName('marker');
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].style.display = 'none';
@@ -8,6 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
 var markers = document.querySelectorAll('.marker');
 
+var crosses = document.querySelectorAll('.close')
 
 markers.forEach(function(marker) {
     marker.addEventListener('click', function() {
@@ -16,10 +17,33 @@ markers.forEach(function(marker) {
     });
 });
 
+
+crosses.forEach(function(marker){
+
+    marker.addEventListener('click', function() {
+        for (var i = 0; i < markerIds.length; i++) {
+            markerIds[i].style.display = 'block';
+        }
+        var allMarkers = document.getElementsByClassName('block-hidden')
+        for (var i = 0; i < allMarkers.length; i++) {
+            allMarkers[i].style.display = 'none';
+        }
+        console.log('Cross clicker')
+        
+    });
+});
+
+
 function handleMarkerClick(markerId) {
     console.log('Marker clicked:', markerId+"-block-hidden");
     battleBlock = document.getElementById(markerId).style.display
-    if (battleBlock == 'none'){document.getElementById(markerId).style.display = 'block'}
+    if (battleBlock == 'none')
+    {
+        document.getElementById(markerId).style.display = 'block'
+    for (var i = 0; i < markerIds.length; i++) {
+        markerIds[i].style.display = 'none';
+    }
+}
     else{
         document.getElementById(markerId).style.display = 'none'
     }
